2fc9fce10cac9f9612789756d5bae3f0a2b9bb86
silvia sendim <sjpsm@iscte-iul.pt>
2018-10-11 19:22:10 +0100

    teste commit

---
+++ BDA_Teste/src/Coordenada.java
@@ -0,0 +1,16 @@
+
+public class Coordenada {
+	private int x;
+	private int y;
+	
+	public Coordenada(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	@Override
+	public String toString() {
+		return "Coordenada [" + x + ", " + y + "]";
+	}
+	
+}
---
+++ BDA_Teste/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Coordenada c = new Coordenada(1,2);
+		System.out.println(c);
+	}
+
+}
