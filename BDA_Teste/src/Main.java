c56541862d79e8af15459530333edf8a8e63fe1e
silvia sendim <sjpsm@iscte-iul.pt>
2018-10-12 20:27:19 +0100

    tes

--- BDA_Teste/src/Main.java
+++ BDA_Teste/src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub1
 		Coordenada c = new Coordenada(1,6);
 		System.out.println(c);
 	}
